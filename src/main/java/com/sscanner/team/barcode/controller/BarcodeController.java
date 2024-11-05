02623cea2d85b9a730f728b439e2bed0a9286ad1
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-11-05 14:09:22 +0900

    Feature/102 swagger (#104)
    * feature: SpringDoc 초기 세팅
    
    * feature: Admin Swagger
    
    * rename: ApiResponse 중복 문제로 BaseApiResponse 로 변경
    
    * feature: barcode swagger
    
    * feature: board swagger
    
    * feature: ErrorApiResponses Annotation 추가
    
    * fix: ErrorApiResponses Annotation 적용
    
    * feature: comment swagger
    
    * feature: gifticon swagger
    
    * feature: payment swagger
    
    * feature: point swagger
    
    * feature: user swagger
    
    * feature: trashcan swagger
    
    * feature: sms swagger
    
    * feature: auth swagger
    
    * feature: product swagger
    
    * feature: trashcan swagger
    
    * refactor: BaseApiResponse 변환
    
    * fix: HOME -> RDS

--- src/main/java/com/sscanner/team/barcode/controller/BarcodeController.java
+++ src/main/java/com/sscanner/team/barcode/controller/BarcodeController.java
@@ -2,7 +2,12 @@ package com.sscanner.team.barcode.controller;
 
 import com.sscanner.team.barcode.responsedto.BarcodeResponseDto;
 import com.sscanner.team.barcode.service.BarcodeService;
-import com.sscanner.team.global.common.response.ApiResponse;
+import com.sscanner.team.global.annotation.ErrorApiResponses;
+import com.sscanner.team.global.common.response.BaseApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +18,16 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/barcodes")
+@ErrorApiResponses
 public class BarcodeController {
 
     private final BarcodeService barcodeService;
 
+    @Operation(summary = "사용자 바코드 목록 조회", description = "로그인한 사용자의 바코드 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "바코드 목록 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = BarcodeResponseDto.class)))
     @GetMapping("/user")
-    public ApiResponse<List<BarcodeResponseDto>> getUserBarcodes() {
+    public BaseApiResponse<List<BarcodeResponseDto>> getUserBarcodes() {
         List<BarcodeResponseDto> barcodes = barcodeService.findBarcodesByUserId();
-        return ApiResponse.ok(200, barcodes, "바코드 목록 조회 성공");
+        return BaseApiResponse.ok(200, barcodes, "바코드 목록 조회 성공");
     }
 }
