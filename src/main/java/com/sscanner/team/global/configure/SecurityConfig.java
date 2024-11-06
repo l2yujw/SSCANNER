5b199b57462c6e2ce4fc830c47d015e736976bd7
김지은 <wldms5119@naver.com>
2024-11-06 10:27:23 +0900

    refactor : 경로별 인가 수정

--- src/main/java/com/sscanner/team/global/configure/SecurityConfig.java
+++ src/main/java/com/sscanner/team/global/configure/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                  // 경로별 인가
                 .authorizeHttpRequests((authorize) ->
-                authorize.requestMatchers("/login", "/", "health", "api/users/join", "/sms/**", "/api/users/reset-password", "/api/users/find-id", "/reissue", "/actuator/**").permitAll()
+                authorize.requestMatchers("/login", "/", "health", "api/users/join", "/sms/**", "/api/users/reset-password", "/api/users/find-id", "/reissue", "/actuator/**","/api/trash/**").permitAll()
                         .requestMatchers("/api/admin/boards/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
